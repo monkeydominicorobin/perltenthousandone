493538d9d9784411f69e1afaccb8e825f1f94640
Rommel Dominguez <bm-2cu8dmto5ga5pf2xkvapdrqcpwtzybsxpw@riseup.net>
2021-09-21 16:38:44 +0800

    adding lines of simple code

--- tenk.pl
+++ tenk.pl
@@ -1,1 +1,26 @@
-
+#!/usr/bin/perl
+sub fone()
+{
+        print "I am the first Perl submodule\n" ;
+}
+sub ftwo()
+{
+        print "I am Perl submodule number two\n" ;
+}
+sub fthree()
+{
+        print "I am Perl submodule number three\n" ;
+}
+sub ffour()
+{
+        print "I am Perl submodule number four\n" ;
+}
+sub ffive()
+{
+        print "I am Perl submodule number five\n" ;
+}
+fone();
+ftwo();
+fthree();
+ffour();
+ffive();
